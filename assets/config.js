d1ece9dfff8e81ed7b66ba2371bdeacd43ca505b
Hetesi Roland <rhetesi@gmail.com>
2023-05-14 17:34:01 +0200

    Edit pricePerKm object by adding legal sentences

--- assets/config.js
+++ assets/config.js
@@ -1,8 +1,16 @@
 const workCity = "Hévíz";
 // const pricePerKm = 30;
 const pricePerKm = {
-  20220101: 15,
-  20230101: 30,
+  20220101: {
+    price: 15,
+    legal:
+      "Az elszámolás alapja a 39/2010. (II. 26.) számú Kormányrendelet, mely alapján gépjárműre fizethető 15,- Ft/km költségtérítés.",
+  },
+  20230101: {
+    price: 30,
+    legal:
+      "Az elszámolás alapja a 39/2010. (II. 26.) és a 16/2023. (I. 27.) számú Kormányrendeletek, mely alapján gépjárműre fizethető 30,- Ft/km költségtérítés.",
+  },
 };
 
 export { workCity, pricePerKm };
